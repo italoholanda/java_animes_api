77e5d7db495910a3e0d61fa4e15b0c8689303f6a
italo <you@example.com>
2022-05-30 13:53:13 -0300

    feature: created anime mapper

--- src/main/java/com/example/demo/mapper/AnimeMapper.java
+++ src/main/java/com/example/demo/mapper/AnimeMapper.java
@@ -1,4 +1,14 @@
 package com.example.demo.mapper;
 
-public class AnimeMapper {
+import com.example.demo.domain.Anime;
+import com.example.demo.request.AnimePostRequestBody;
+import com.example.demo.request.AnimePutRequestBody;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public abstract class AnimeMapper {
+    public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+    public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
+    public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
 }
