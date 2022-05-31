54d217a4872f03dc97f8712415f940179c0a6537
italo <italogholanda@gmail.com>
2022-05-31 06:49:41 -0300

    feat: 'required name field' post validation

--- src/main/java/com/example/demo/request/AnimePostRequestBody.java
+++ src/main/java/com/example/demo/request/AnimePostRequestBody.java
@@ -2,7 +2,10 @@ package com.example.demo.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "Name field cannot be empty")
     private String name;
 }
